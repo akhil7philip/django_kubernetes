457bf5b53c9f99744bb2924588d4fceef9a40f53
Akhil <akhil7philip@gmail.com>
2022-10-28 20:24:18 +0530

    spaces added

--- django/polls/tests.py
+++ django/polls/tests.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
 
-from .models import PollsModel
+# from .models import PollsModel
 
-# Create your tests here.
-class PollsTest (TestCase):
-    def setUp(self):
-        PollsModel.objects.create(name='Aks')
+# # Create your tests here.
+# class PollsTest (TestCase):
+#     def setUp(self):
+#         PollsModel.objects.create(name='Aks')
         
-    def test_case(self):
-        q = PollsModel.objects.all()
-        self.assertTrue(q.exists())
+#     def test_case(self):
+#         q = PollsModel.objects.all()
+#         self.assertTrue(q.exists())
