3e26e58b6e7b90cd4a0f3fb0c573b14c07fad385
Akhil <akhil7philip@gmail.com>
2022-10-28 20:32:07 +0530

    changes 15

--- django/migrate.sh
+++ django/migrate.sh
@@ -3,6 +3,6 @@
 SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"admin@admin.com"}
 cd /app/
 
-python manage.py collectstatic 
+python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
